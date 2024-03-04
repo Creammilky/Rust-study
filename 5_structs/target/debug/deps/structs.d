259c91b26f233663a5e507b93a4228cb7ddc4dfd
Creammilky <Cream_milk@bupt.edu.cn>
2024-03-04 22:37:24 +0800

    added struct example

---
+++ 5_structs/target/debug/deps/structs.d
@@ -0,0 +1,5 @@
+D:\BUPT++\Rust\5_structs\target\debug\deps\structs.exe: src\main.rs
+
+D:\BUPT++\Rust\5_structs\target\debug\deps\structs.d: src\main.rs
+
+src\main.rs:
---
+++ 5_structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+D:\BUPT++\Rust\5_structs\target\debug\structs.exe: D:\BUPT++\Rust\5_structs\src\main.rs
