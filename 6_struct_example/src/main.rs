106d310a51ce311be201b5aa396d3e2880a11458
Creammilky <Cream_milk@bupt.edu.cn>
2024-03-05 17:43:56 +0800

    Implemented struct example

--- 6_struct_example/src/main.rs
+++ 6_struct_example/src/main.rs
@@ -1,3 +1,47 @@
+#[derive(Debug)] // Allow debug info output
+struct Rectangle{
+    width: u32,
+    height: u32,
+    area: u32   // 0 represents not calculated
+}
+
+impl Rectangle  {
+    fn get_area(&self) -> u32{
+        self.area
+    }
+
+    fn calc_area(&self) -> u32{
+        self.width * self.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rec1 = Rectangle{
+        width: 30,
+        height : 50,
+        area: 0,
+    };
+    let rec2 = get_rectangle(20,70);
+    println!("{:?}", rec1);
+    println!("{:#?}", rec1);
+
+    println!("Area1: {}", get_area_by_calc(&rec1));
+    println!("Area2: {}", rec2.calc_area());
+}
+
+fn get_rectangle(width: u32, height: u32) -> Rectangle{
+    Rectangle{
+        width,
+        height,
+        area: width * height,
+    }
+}
+
+// Will it cause an overflow?
+fn get_area(rec: &Rectangle) -> u32{
+    rec.area
+}
+
+fn get_area_by_calc(rec: &Rectangle) -> u32 {
+    rec.height * rec.width
 }
